0e4b978b14242302cc3ae89e7f50587bd935be34
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2024-05-10 20:33:40 +0700

    Update install_packages.sh

--- installers/install_packages.sh
+++ installers/install_packages.sh
@@ -4,7 +4,7 @@ sudo apt install git tree wget zip cmake build-essential htop net-tools -y
 sudo apt install zsh tmux curl openssh-server sshfs gpg jq python3 shellcheck -y
 sudo apt install ffmpeg openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
 sudo apt install libfreetype6-dev libevent-dev ncurses-dev bison pkg-config -y
-sudo apt install libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev -y
+sudo apt install libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev libfuse2 -y
 
 # TODO: to be moved
 sudo snap install discord btop
@@ -45,12 +45,13 @@ gh extension install github/gh-copilot --force
 
 
 # bat
-# echo 'export PATH="$PATH:~/.local/bin"' >> ~/.bashrc
+
 sudo apt install bat -y
 mkdir -p ~/.local/bin
 rm ~/.local/bin/bat
 ln -s /usr/bin/batcat ~/.local/bin/bat
 source ~/.bashrc
+export PATH="$PATH:~/.local/bin"
 
 # theme fot bat
 mkdir -p "$(bat --config-dir)/themes"
