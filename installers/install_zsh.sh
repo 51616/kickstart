8037ad1f88c63ac63d25486ce30467781cf69c76
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2024-08-21 20:30:13 +0700

    Update install_zsh.sh

--- installers/install_zsh.sh
+++ installers/install_zsh.sh
@@ -2,7 +2,7 @@
 
 # install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
 
 # download 10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
