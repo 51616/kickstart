5f935935a5685752041975677fd94d469a109b63
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2024-09-23 20:18:10 +0700

    Update install_nerd_font.sh

--- installers/install_nerd_font.sh
+++ installers/install_nerd_font.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install the fonts
-# "AnonymousPro" "Ubuntu" "UbuntuMono" "Noto" "RobotoMono" "SourceCodePro" "Hack" "DejaVuSansMono"
-fonts_to_install=("FiraCode" "FiraMono")
+# "AnonymousPro" "Noto" "RobotoMono" "SourceCodePro" "Hack" "DejaVuSansMono"
+fonts_to_install=("FiraCode" "FiraMono" "Ubuntu" "UbuntuMono")
 
 git clone --sparse --depth=1 --filter=blob:none https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
