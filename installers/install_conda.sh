ada9a8c4a97de56e59b9c958792585d25ec7419c
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2025-03-18 15:33:37 +0700

    Update install_conda.sh

--- installers/install_conda.sh
+++ installers/install_conda.sh
@@ -4,6 +4,6 @@ bash Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 conda update -n base conda
 conda install -n base python=3.11
-conda install -n base conda-libmamba-solver
-conda config --set solver libmamba
+# conda install -n base conda-libmamba-solver
+# conda config --set solver libmamba
 rm Miniconda3-latest-Linux-x86_64.sh
