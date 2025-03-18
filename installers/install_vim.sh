3f80479487b60a327174bc6b55d2a8db4bbc94db
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2025-03-18 17:10:56 +0700

    Update install_vim.sh

--- installers/install_vim.sh
+++ installers/install_vim.sh
@@ -4,7 +4,7 @@ chmod u+x nvim.appimage
 
 sudo mkdir -p /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
-ln -s /opt/nvim/nvim ~/.local/bin/nvim
+sudo ln -s /opt/nvim/nvim ~/.local/bin/nvim
 
 # just use bare nvchad config
 rm -rf ~/.local/share/nvim/
