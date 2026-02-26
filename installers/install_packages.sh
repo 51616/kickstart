64ef5ea22fd7ec6223967a6c47a3b0f8f28a7eee
Rujikorn Charakorn <rujikorn.ch@gmail.com>
2026-02-27 00:09:53 +0900

    Install pipx and additional command line tools
    Added installation of pipx and several CLI tools.

--- installers/install_packages.sh
+++ installers/install_packages.sh
@@ -8,3 +8,11 @@ sudo apt install ffmpeg openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y
 sudo apt install libfreetype6-dev libevent-dev ncurses-dev bison pkg-config -y
 sudo apt install libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev libfuse2 -y
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sudo apt install pipx -y
+pipx ensurepath
+pipx install rich-cli
+pipx install gdown
+pipx install tldr
+pipx install yt-dlp
+source ~/.bashrc
